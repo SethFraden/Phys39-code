72ddd71d2fb5caca9fddf13ef45c35cee3854593
SethFraden <143132246+SethFraden@users.noreply.github.com>
2023-10-09 13:15:33 -0400

    (3) Models that the thermistor block has a thermal mass. This introduces a phase lag between the TEC temperature and the thermistor temperature.
    Performs a model of
    
    (1) Manual Control with a heat loss term and a TEC heating/cooling term
    (2) Proportional Control with a heat loss term and the TEC heating/cooling term is now proportional to the difference in the temperature of the system and the room.

---
+++ ThermistorTimeSerialOutForMatlab_20230930/ThermistorTimeSerialOutForMatlab_20230930.ino
@@ -0,0 +1,103 @@
+
+// September 29, 2023
+//Works!
+
+// Reads voltage from voltage divider with thermistor as one leg. Averages numMeasurements times.
+// Thermistor is in leg next to ground and the voltage is read by "tempIn"
+// The thermistor is a Epcos, R/T 2014 with R_25 = 100,000 ohms. B-parameter equation given in code.
+// Outputs temperature (C), elapsed time (s) and PWM (0-255)
+// Example serial line output:
+//              Temperature (C): 21.13, Time (s): 13.89, Heat/Cool: 1, PWM: 129
+
+
+// Works with Matlab app "testappV3"
+// Matlab reads and plots the results in a stripchart
+// Matlab sets pwmValue between [0,255] and passes value to Arduino via Serial
+// Arduino code uses the pwmValue to control H-bridge
+// Matlab sets HeatCool as either 0 (cool) or 1 (heat) and passes value to Arduino via Serial
+
+
+const int tempIn = A0;  // Analog input pin
+const int numMeasurements = 5000;  // Number of measurements
+
+// power
+#define pwmOut1 9
+#define pwmOut2 10
+
+
+// Fixed resistor value in ohms
+// const float fixedResistance = 4670.0; // my board
+const float fixedResistance = 100000.0; // board in 39 lab
+
+// Thermistor parameters for 100k ohm 
+const float beta = 4540.0;
+const float R25 = 100000.0;
+const float T0 = 298.15;  // 25°C in Kelvin
+int  pwmValue = 0;
+int  HeatCool = 1;
+
+void setup() {
+  Serial.begin(9600);  // Initialize serial communication
+  while (!Serial) {
+    ;  // Wait for serial port to connect
+  }
+}
+unsigned long startTime = millis();  // Get the starting time
+
+void loop() {
+  
+
+  // Perform measurements and calculate average
+float total = 0;
+  for (int i = 0; i < numMeasurements; i++) {
+    int sensorValue = analogRead(tempIn);
+    total = total + sensorValue;
+  }
+float average = total / (float)numMeasurements;
+
+    // Convert the analog value to voltage
+    float voltage = average * (5.0 / 1023.0);  // Assuming 5V reference voltage
+
+    // Calculate the resistance of the thermistor
+    float thermistorResistance = (fixedResistance * voltage) / (5.0 - voltage);
+
+    // Calculate the temperature using the Steinhart-Hart equation. See Wiki Thermistor entry.
+    float steinhart = log(thermistorResistance / R25);
+    steinhart /= beta;
+    steinhart += 1.0 / T0;
+    float temperature = 1.0 / steinhart - 273.15;  // temperature in Centigrade
+   
+// Matlab sends pwm and heat/cool information over the serial line
+ if (Serial.available() > 0) {
+    String command = Serial.readStringUntil('\n');
+    
+    if (command.startsWith("PWM")) {
+      sscanf(command.c_str(), "PWM %d", &pwmValue);
+    }
+
+    if (command.startsWith("Heat/Cool")) {
+      sscanf(command.c_str(), "Heat/Cool %d", &HeatCool);
+    }
+    if (HeatCool == 1){
+      analogWrite(pwmOut1, pwmValue);
+      analogWrite(pwmOut2,0); 
+    }
+    if (HeatCool == 0){
+      analogWrite(pwmOut2, pwmValue);
+      analogWrite(pwmOut1,0); 
+    }
+ 
+  }
+
+  // Print average temperature and time in seconds
+  unsigned long currentTime = millis();
+  float elapsedTime = (currentTime - startTime) / 1000.0;
+  Serial.print("Temperature (C): ");
+  Serial.print(temperature);
+  Serial.print(", Time (s): ");
+  Serial.print(elapsedTime);
+  Serial.print(", Heat/Cool: ");
+  Serial.print(HeatCool);
+  Serial.print(", PWM: ");
+  Serial.println(pwmValue);
+}
